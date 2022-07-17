c9102dc0fce9758c6f2921a1c948c1b13c110ab8
Max Stabel <M4a1x@users.noreply.github.com>
2022-07-17 15:41:06 +0200

    Create function to compare weight based MOO with pareto based MOO
    Get rid of mga_* visualization notebooks, as they all do the same with
    different test functions. this can now all be done with __main__.py

--- omfe/__main__.py
+++ omfe/__main__.py
@@ -1,35 +1,65 @@
 import omfe
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from omfe.evaluator import Evaluator, NonDominatedSortEvaluator, WeightBasedEvaluator
+
+from omfe.mga import AlgorithmRunner, MicroGeneticAlgorithm
+from omfe.problems import CTP1, BinhKorn, ChankongHaimes, Kursawe, Problem, plot_agents
 
 
 def main():
-    simple_plot()
-    tournament()
+    compare_plots(BinhKorn())
+    compare_plots(ChankongHaimes())
+    compare_plots(CTP1())
+    compare_plots(Kursawe())
+    plt.show()
 
 
-def simple_plot():
-    seed = 2
-    problem = omfe.problems.BinhKorn()
-    evaluator = omfe.evaluator.NonDominatedSortEvaluator(problem)
-    mga = omfe.mga.MicroGeneticAlgorithm(
-        problem=problem,
-        evaluator=evaluator,
+def compare_plots(problem: Problem, seed=42):
+    # Reduce MOO problem to SOO problem by using weights for different
+    # objective functions to obtain single solution.
+    # Run multiple times with random weights to explore MOO objective space
+    weight_evaluator = WeightBasedEvaluator(problem, seed)
+    mga_weight = MicroGeneticAlgorithm(
+        problem,
+        weight_evaluator,
         population_size=5,
         agents_to_keep=1,
-        max_iterations=200,
+        max_iterations=50,
         num_bits=8,
         seed=seed,
     )
-    agents_history = mga.run()
+    runner = AlgorithmRunner(mga_weight, seed)
+    times = 200
+    best_agents = runner.run(times)
 
-    fig, ax = plt.subplots()  # Convenience method to create figure and plot
-    for agents in agents_history:
-        omfe.benchmark.plot_agents(
-            axes=ax, agents=agents, problem=mga.problem, marker="o"
-        )
-        fig.show()
-    plt.show(block=True)
+    # Now run MOO problem with Pareto front based ranking to find multiple
+    # solutions at the same time
+    pareto_evaluator = NonDominatedSortEvaluator(problem)
+    mga_pareto = MicroGeneticAlgorithm(
+        problem,
+        pareto_evaluator,
+        population_size=201,
+        agents_to_keep=1,
+        max_iterations=50,
+        num_bits=8,
+        seed=seed,
+    )
+    final_population = mga_pareto.run()[-1]
+
+    # Plot both next to each other
+    fig, ax = plt.subplots(1, 2)
+    plot_agents(ax[0], best_agents, problem)
+    problem.plot_pareto_front(ax[0])
+    ax[0].set_title(
+        f"{problem} SOO with random weights. Population Size: {mga_weight.population_size}, Random Restarts: {times}"
+    )
+
+    plot_agents(ax[1], final_population, problem)
+    problem.plot_pareto_front(ax[1])
+    ax[1].set_title(
+        f"{problem} MOO with pareto front ranking. Population Size: {mga_pareto.population_size}"
+    )
 
 
 def tournament():
