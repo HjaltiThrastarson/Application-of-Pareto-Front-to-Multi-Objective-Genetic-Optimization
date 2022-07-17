b8145a6e210a2564aecb737e8886ee921b0d9243
Max Stabel <M4a1x@users.noreply.github.com>
2022-07-17 14:55:21 +0200

    Add vectorized function version to problems
    Added helper function to plot pareto fronts as well

--- omfe/problems.py
+++ omfe/problems.py
@@ -1,23 +1,34 @@
 """Module containing various MOO test problems"""
+from abc import ABC, abstractmethod
 from typing import Callable, Sequence
 import numpy as np
 import numpy.typing as npt
+import matplotlib.pyplot as plt
+
+from omfe.benchmark import (
+    plot_binh_korn_pareto_front,
+    plot_chankong_haimes_pareto_front,
+    plot_ctp1_pareto_front,
+    plot_kursawe_pareto_front,
+)
 
 # TODO: If too slow, vectorize evaluate_functions and is_inside_constraints
 # i.e. implement it for single agents and lists of agents directly for a problem
 # instead of a general iterator
 
 
-class Problem:
+class Problem(ABC):
     """Default problem class consisting of functions, constraints and the search domain"""
 
     def __init__(
         self,
         functions: Sequence[Callable],
+        functions_vec: Sequence[Callable],
         constraints: Sequence[Callable],
         search_domain: Sequence[Sequence[int]],
     ) -> None:
         self.functions = functions
+        self.functions_vec = functions_vec
         self.constraints = constraints
         self.search_domain = search_domain
 
@@ -38,6 +49,11 @@ class Problem:
         """Returns True if any number of constraints is broken"""
         return not self.is_inside_constraints(agent)
 
+    @staticmethod
+    @abstractmethod
+    def plot_pareto_front(axes: plt.Axes, *args, **kwargs):
+        pass
+
     @property
     def num_variables(self) -> int:
         return len(self.search_domain)
@@ -47,10 +63,33 @@ class Problem:
         return len(self.functions)
 
 
+def plot_agents(
+    axes: plt.Axes,
+    agents: npt.NDArray[np.float64],
+    problem: Problem,
+    *args,
+    **kwargs,
+):
+    """Takes a list of agents for a given problem and plots them, returning the plot"""
+
+    # TODO: Highlight current front! use pareto_front from evaluator for this
+    # TODO: Move this to another more fitting module
+
+    F = np.empty((len(agents), 2))
+    for i, agent in enumerate(agents):
+        F[i] = problem.evaluate_functions(agent)
+
+    axes.set_xlabel("f1")
+    axes.set_ylabel("f2")
+    axes.set_title(f"Objective Space Plot: {problem}")
+    return axes.scatter(F[:, 0], F[:, 1], *args, **kwargs)
+
+
 class ChankongHaimes(Problem):
     def __init__(self) -> None:
         super().__init__(
             functions=[self.f_1, self.f_2],
+            functions_vec=[self.f_1_vec, self.f_2_vec],
             constraints=[self.g_1, self.g_2],
             search_domain=[[-20, 20], [-20, 20]],
         )
@@ -62,12 +101,24 @@ class ChankongHaimes(Problem):
         return "Chankong and Haimes function"
 
     @staticmethod
+    def plot_pareto_front(axes: plt.Axes, *args, **kwargs):
+        return plot_chankong_haimes_pareto_front(axes, *args, **kwargs)
+
+    @staticmethod
     def f_1(agent: Sequence[float]) -> float:
         return 2 + (agent[0] - 2) ** 2 + (agent[1] - 1) ** 2
 
     @staticmethod
+    def f_1_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return 2 + (agents[:, 0] - 2) ** 2 + (agents[:, 1] - 1) ** 2
+
+    @staticmethod
     def f_2(agent: Sequence[float]) -> float:
         return 9 * agent[0] - (agent[1] - 1) ** 2
+
+    @staticmethod
+    def f_2_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return 9 * agents[:, 0] - (agents[:, 1] - 1) ** 2
 
     @staticmethod
     def g_1(agent: Sequence[float]) -> bool:
@@ -82,6 +133,7 @@ class BinhKorn(Problem):
     def __init__(self) -> None:
         super().__init__(
             functions=[self.f_1, self.f_2],
+            functions_vec=[self.f_1_vec, self.f_2_vec],
             constraints=[self.g_1, self.g_2],
             search_domain=[[0, 5], [0, 3]],
         )
@@ -93,12 +145,24 @@ class BinhKorn(Problem):
         return "Binh and Korn function"
 
     @staticmethod
+    def plot_pareto_front(axes: plt.Axes, *args, **kwargs):
+        return plot_binh_korn_pareto_front(axes, *args, **kwargs)
+
+    @staticmethod
     def f_1(agent: Sequence[float]) -> float:
         return 4 * (agent[0] ** 2) + 4 * (agent[1] ** 2)
 
     @staticmethod
+    def f_1_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return 4 * (agents[:, 0] ** 2) + 4 * (agents[:, 1] ** 2)
+
+    @staticmethod
     def f_2(agent: Sequence[float]) -> float:
         return (agent[0] - 5) ** 2 + (agent[1] - 5) ** 2
+
+    @staticmethod
+    def f_2_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return (agents[:, 0] - 5) ** 2 + (agents[:, 1] - 5) ** 2
 
     @staticmethod
     def g_1(agent: Sequence[float]) -> bool:
@@ -113,6 +177,7 @@ class Kursawe(Problem):
     def __init__(self) -> None:
         super().__init__(
             functions=[self.f_1, self.f_2],
+            functions_vec=[self.f_1_vec, self.f_2_vec],
             constraints=[],
             search_domain=[[-5, 5], [-5, 5], [-5, 5]],
         )
@@ -124,9 +189,19 @@ class Kursawe(Problem):
         return "Kursawe function"
 
     @staticmethod
+    def plot_pareto_front(axes: plt.Axes, *args, **kwargs):
+        return plot_kursawe_pareto_front(axes, *args, **kwargs)
+
+    @staticmethod
     def f_1(agent: Sequence[float]) -> float:
         return (-10 * np.exp(-0.2 * np.sqrt(agent[0] ** 2 + agent[1] ** 2))) + (
             -10 * np.exp(-0.2 * np.sqrt(agent[1] ** 2 + agent[2] ** 2))
+        )
+
+    @staticmethod
+    def f_1_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return (-10 * np.exp(-0.2 * np.sqrt(agents[:, 0] ** 2 + agents[:, 1] ** 2))) + (
+            -10 * np.exp(-0.2 * np.sqrt(agents[:, 1] ** 2 + agents[:, 2] ** 2))
         )
 
     @staticmethod
@@ -137,11 +212,20 @@ class Kursawe(Problem):
             + (np.abs(agent[2]) ** 0.8 + 5 * np.sin(agent[2] ** 3))
         )
 
+    @staticmethod
+    def f_2_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return (
+            (np.abs(agents[:, 0]) ** 0.8 + 5 * np.sin(agents[:, 0] ** 3))
+            + (np.abs(agents[:, 1]) ** 0.8 + 5 * np.sin(agents[:, 1] ** 3))
+            + (np.abs(agents[:, 2]) ** 0.8 + 5 * np.sin(agents[:, 2] ** 3))
+        )
+
 
 class CTP1(Problem):
     def __init__(self) -> None:
         super().__init__(
             functions=[self.f_1, self.f_2],
+            functions_vec=[self.f_1_vec, self.f_2_vec],
             constraints=[self.g_1, self.g_2],
             search_domain=[[0, 1], [0, 1]],
         )
@@ -150,15 +234,27 @@ class CTP1(Problem):
         return "CTP1"
 
     def __str__(self) -> str:
-        return "CTP1"
+        return "CTP1 function"
+
+    @staticmethod
+    def plot_pareto_front(axes: plt.Axes, *args, **kwargs):
+        return plot_ctp1_pareto_front(axes, *args, **kwargs)
 
     @staticmethod
     def f_1(agent: Sequence[float]) -> float:
         return agent[0]
 
     @staticmethod
+    def f_1_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return agents[:, 0]
+
+    @staticmethod
     def f_2(agent: Sequence[float]) -> float:
         return (1 + agent[1]) * np.exp(-agent[0] / (1 + agent[1]))
+
+    @staticmethod
+    def f_2_vec(agents: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+        return (1 + agents[:, 1]) * np.exp(-agents[:, 0] / (1 + agents[:, 1]))
 
     @staticmethod
     def g_1(agent: Sequence[float]) -> float:
